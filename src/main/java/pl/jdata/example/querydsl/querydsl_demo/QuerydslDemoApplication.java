176393f02924dbc03f0d18f383edaf48a12b09c5
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2018-10-25 20:20:37 +0200

    Initial commit.

---
+++ src/main/java/pl/jdata/example/querydsl/querydsl_demo/QuerydslDemoApplication.java
@@ -0,0 +1,12 @@
+package pl.jdata.example.querydsl.querydsl_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslDemoApplication.class, args);
+    }
+}
